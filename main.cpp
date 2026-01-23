b79273a095c2e1e4349e452f19b8751f2f1de2af
MorzBNG <morzbng@gmail.com>
2026-01-23 21:38:51 +0100

    Added .gitignore file and first changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
+    //handle arguments
+    //getopt 
+    //print software signature
+    std::cout << "Wieselcat V0.0.1\n\nTickleTheShark Inc.\nby MorzBNG\n";
     return 1;
 }
